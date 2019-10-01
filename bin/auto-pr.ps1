5dfa704142524fbef33ec1f44c847bc525845e9a
Rafael Borges <rafael@prothos.com.br>
2019-10-01 15:52:44 -0300

    Updated binaries

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,9 +1,9 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "hermanjustnu/scoop-emulators:master"
+    [String]$upstream = "borger/scoop-emulators:master"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
 $autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
 $dir = "$psscriptroot/.." # checks the parent dir
-iex -command "$autopr -dir $dir -upstream $upstream $($args |% { "$_ " })"
+Invoke-Expression -command "& '$autopr' -dir '$dir' -upstream $upstream $($args | ForEach-Object { "$_ " })"
