11551e44045d8ef0c65f591132c65fc66b3ae2d1
beyondmeat <51850644+beyondmeat@users.noreply.github.com>
2022-07-09 19:14:27 -0700

    fix

--- bin/test.ps1
+++ bin/test.ps1
@@ -1,3 +1,5 @@
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-$result = Invoke-Pester "$psscriptroot/.." -PassThru
+#Requires -Modules @{ ModuleName = 'Pester'; MaximumVersion = '4.99' }
+
+if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
+$result = Invoke-Pester "$PSScriptRoot\.." -PassThru
 exit $result.FailedCount
